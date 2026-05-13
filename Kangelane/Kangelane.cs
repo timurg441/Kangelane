a19524b0cf69871b13d5416efafff94cfa8a22a5
opilane <opilane@HP-CZC2349HT4>
2026-05-13 15:40:42 +0300

    .

---
+++ Kangelane/Kangelane.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// klass 1 Kangelane (baasklass)
+class Kangelane
+{
+    // Privaatsed isendiväljad
+    private string nimi;
+    private string asukoht;
+
+    // Konstruktor
+    public Kangelane(string nimi, string asukoht)
+    {
+        this.nimi = nimi;
+        this.asukoht = asukoht;
+    }
+
+    // Omadused (get/set)
+    public string Nimi
+    {
+        get { return nimi; }
+        set { nimi = value; }
+    }
+
+    public string Asukoht
+    {
+        get { return asukoht; }
+        set { asukoht = value; }
+    }
+
+    // Tagastab 95% ohus olevatest inimestest (ümardatult)
+    public virtual int Paasta(int ohus)
+    {
+        return (int)Math.Round(ohus * 0.95);
+    }
+
+    // Tagastab sõne kangelase riietuse kohta
+    public virtual string Vormiriietus()
+    {
+        return nimi + " kannab lihtsat kangelase kostüümi ja maski.";
+    }
+
+    // Isikupärane tervitus
+    public virtual string Tervitus()
+    {
+        return "Tere! Mina olen " + nimi + " ja kaitsen " + asukoht + " linna!";
+    }
+
+    // Annab teada, kas kangelane on saadaval
+    public virtual string MissiooniStaatus()
+    {
+        return nimi + " on saadaval ja valmis aitama!";
+    }
+
+    // Tagastab kangelase kirjelduse
+    public override string ToString()
+    {
+        return "[Kangelane] Nimi: " + nimi + " | Linn: " + asukoht;
+    }
+}
---
+++ Kangelane/SuperKangelane.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// klass 2: SuperKangelane (pärib Kangelane)
+class SuperKangelane : Kangelane
+{
+    // Lisaomadus: osavus vahemikus [1.0, 5.0)
+    private double osavus;
+
+    // Konstruktor — osavus määratakse juhuslikult
+    public SuperKangelane(string nimi, string asukoht) : base(nimi, asukoht)
+    {
+        Random rand = new Random();
+        // NextDouble() annab [0.0, 1.0) → tulemus on [1.0, 5.0)
+        osavus = Math.Round(1.0 + rand.NextDouble() * 4.0, 2);
+        if (osavus >= 5.0) osavus = 4.99; // garanteerime ülempiiri
+    }
+
+    // Osavuse omadus (ainult get, sest määratakse konstruktoris)
+    public double Osavus
+    {
+        get { return osavus; }
+    }
+
+    // Päästab (95 + osavus)% inimesi
+    public override int Paasta(int ohus)
+    {
+        double protsent = (95.0 + osavus) / 100.0;
+        return (int)Math.Round(ohus * protsent);
+    }
+
+    // Eriline superkangelase kostüüm
+    public override string Vormiriietus()
+    {
+        return Nimi + " kannab säravat superkangelase kostüümi keebi ja logoga!";
+    }
+
+    // Efektne tervitus
+    public override string Tervitus()
+    {
+        return "*** " + Nimi + " siin! " + Asukoht + " on minu kaitse all - kurjategijad, olge ettevaatlikud! ***";
+    }
+
+    // Superkangelane on sageli juba missioonil
+    public override string MissiooniStaatus()
+    {
+        Random rand = new Random();
+        if (rand.Next(2) == 0)
+            return Nimi + " on hetkel missioonil - tagastab varsti!";
+        else
+            return Nimi + " on saadaval ja kõrgendatud valmisolekus!";
+    }
+
+    // Lisab osavuse info
+    public override string ToString()
+    {
+        return "[SuperKangelane] Nimi: " + Nimi + " | Linn: " + Asukoht + " | Osavus: " + osavus;
+    }
+}
